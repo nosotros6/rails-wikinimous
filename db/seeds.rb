35b78227cb53ebce8bbaebe7ffd83985e53d8166
pilar garcia <pilargarciaroyo@gmail.com>
2021-07-22 13:00:28 -0400

    wiki incompleto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+Article.destroy_all
+10.times do
+  Article.create(title: Faker::Fantasy::Tolkien.character, content: Faker::Fantasy::Tolkien.poem )
+end
